299d34e4dca16f2f7f5893ba45a9e7e746740c0d
zgghc <zgghc@126.com>
2019-11-11 14:58:09 +0800

    change 404

--- pcWebpack/src/views/404.vue
+++ pcWebpack/src/views/404.vue
@@ -8,13 +8,12 @@
         <img class="pic-404__child right" src="@/assets/images/404_images/404_cloud.png" alt="404">
       </div>
       <div class="bullshit">
-        <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">All rights reserved
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a>
+        <div class="bullshit__oops">404 错误</div>
+        <div class="bullshit__info">
         </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-        <a href="" class="bullshit__return-home">Back to home</a>
+        <div class="bullshit__info">请检查您输入的url是否正确, 或点击下面的返回按钮。</div>
+        <a href="" class="bullshit__return-home">返回</a>
       </div>
     </div>
   </div>
@@ -26,7 +25,7 @@ export default {
   name: 'Page404',
   computed: {
     message() {
-      return 'The webmaster said that you can not enter this page...'
+      return '网页无法找到！'
     }
   }
 }
