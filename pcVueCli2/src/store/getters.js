8c1f041621cfc02d38c5d0babb2bd5131ba53410
zgghc <zgghc@126.com>
2019-11-14 15:43:39 +0800

    del cli2

--- pcVueCli2/src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  menuPos : state => state.app.menuPos,
-  visitedViews: state => state.tagsView.visitedViews,
-  cachedViews: state => state.tagsView.cachedViews,
-}
-export default getters
